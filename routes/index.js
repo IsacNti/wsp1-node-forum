093422e281754706f1f5f58808fc3ca4c70a6993
IsacNti <isac.lindholm@elev.ga.ntig.se>
2023-01-27 10:48:00 +0100

    del 8 typ lite fixad

--- routes/index.js
+++ routes/index.js
@@ -15,21 +15,32 @@ const promisePool = pool.promise();
 
 router.get('/', async function (req, res, next) {
     const [rows] = await promisePool.query("SELECT * FROM il05forum");
-res.render('index.njk', {
-    rows: rows,
-    title: 'Forum',
+    res.render('index.njk', {
+        rows: rows,
+        title: 'Forum',
     });
 });
 
 router.get('/new', async function (req, res, next) {
+    const [users] = await promisePool.query("SELECT * FROM il05users");
     res.render('new.njk', {
         title: 'Nytt inlägg',
+        users,
     });
 });
 
 router.post('/new', async function (req, res, next) {
     const { author, title, content } = req.body;
-    const [rows] = await promisePool.query("INSERT INTO il05forum (author, title, content) VALUES (?, ?, ?)", [author, title, content]);
-    res.redirect('/');
+
+    let user = await promisePool.query('SELECT * FROM il05users WHERE name = ?', [author]);
+    if (!user) {
+        user = await promisePool.query('INSERT INTO il05users (name) VALUES (?)', [author]);
+    }
+
+    const userId = user.insertId || user[0][0].id;
+    const [rows] = await promisePool.query('INSERT INTO il05forum (authorId, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
 });
+
+
 
